35303da4f9be9f90b40d57070589f517af9c8ad4
wenjialu3477 <incablosour@yahoo.co.jp>
2024-10-21 23:17:13 +0900

    Update setSearchingJobCondtions.spec.ts

--- tests/setSearchingJobCondtions.spec.ts
+++ tests/setSearchingJobCondtions.spec.ts
@@ -45,7 +45,7 @@ test('希望求人条件の設定', async ({ page }) => {
   await button.click();
   const savebutton = page.getByRole('button', { name: '保存' });
   await savebutton.waitFor();
-  await page.getByRole('button', { name:  '+ 希望職種を選択' }).click();
+    await page.locator('//*[@id="__next"]/section/section/main/div/div[3]/div[2]/div/section/form/div[1]/section[1]/div/button').click();
   await page.getByLabel('希望職種を選択してください（最大3つまで）').getByPlaceholder('選択してください').click();
   await page.getByRole('option', { name: jobOptionName, exact: true }).click();
   await page.getByLabel(jobLabelName).check();
